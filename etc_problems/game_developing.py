ca74940199a518185169209a5624de84b3aa4e85
hyengeunY <hgyoon0928@gmail.com>
2021-10-23 22:48:35 +0900

    21-10-23 ing

---
+++ etc_problems/game_developing.py
@@ -0,0 +1,47 @@
+# 이것이 코딩테스트다 구현 실전문제 3 개임개발.
+
+if __name__ == "__main__":
+    max_row, max_col = map(int, input().split())
+    visited = [[0] * max_col for _ in range(max_row)]
+
+    y, x, direction = map(int, input().split())
+    visited[y][x] = 1
+
+    map_ = []
+    for i in range(max_row):
+        map_.append(list(map(int, input().split())))
+
+    # 이동가능 방향
+
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
+
+    count = 0
+
+    max_turn = 4
+    num_turn = 0
+
+    while True:
+
+        direction -= 1
+        if direction == -1:
+            direction = 3
+
+        x_temp = x + dx[direction]
+        y_temp = y + dy[direction]
+
+        if visited[y_temp][x_temp] == 0 and map_[y_temp][x_temp] == 0:  # 방문 안함 & 육지
+            visited[y_temp][x_temp] = 1
+            y = y_temp
+            x = x_temp
+            count += 1
+            num_turn = 0
+
+            continue
+
+        num_turn += 1
+
+        if num_turn == 4:
+            break
+
+    print(count)
--- etc_problems/knight_in_castle.py
+++ etc_problems/knight_in_castle.py
@@ -1,4 +1,43 @@
 # 이것이 코딩테스트다 구현 예제 3
 
 if __name__ in "__main__":
+    input = input()
+
+    row = input[1]
+    col = input[0]
+
+    col_boundary = ["a", "h"]
+    row_boundary = ["1", "8"]
+
+    count = 0
+
+    if ord(col) + 2 <= ord("h"):
+        if int(row) + 1 <= 8:
+            count += 1
+        if int(row) - 1 >= 1:
+            count += 1
+    if ord(col) - 2 >= ord("a"):
+        if int(row) + 1 <= 8:
+            count += 1
+        if int(row) - 1 >= 1:
+            count += 1
     
+
+    if int(row) + 2 <= 8:
+        if ord(col) + 1 <= ord("h"):
+            count += 1
+        if ord(col) - 1 >= ord("a"):
+            count += 1
+    if int(row) - 2 >= 1:
+        if ord(col) + 1 <= ord("h"):
+            count += 1
+        if ord(col) - 1 >= ord("a"):
+            count += 1
+
+    
+    print(count)
+    
+    
+
+
+
