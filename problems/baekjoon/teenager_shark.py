2ceac0cd3602b0ebd77d1efed1b4593b605fcb68
HyeonGeunY <hgyoon0928@gmail.com>
2022-06-14 12:30:38 +0900

    teenager shark

--- problems/baekjoon/teenager_shark.py
+++ problems/baekjoon/teenager_shark.py
@@ -2,6 +2,7 @@ move = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1)]
 
 import logging
 from collections import defaultdict
+import copy
 
 simple_formatter = logging.Formatter("[%(name)s] %(message)s")
 console_handler = logging.StreamHandler()
@@ -12,6 +13,7 @@ root_logger = logging.getLogger()
 root_logger.addHandler(console_handler)
 root_logger.setLevel(logging.INFO)
 
+result = 0
 
 def default_value():
     return (-1, -1)
@@ -29,7 +31,7 @@ def get_input():
     return graph
 
 
-def move_fish(graph):
+def move_fish(graph):  
     fish_dic = defaultdict(default_value)
 
     for i in range(4):
@@ -69,19 +71,49 @@ def move_fish(graph):
 
     return graph
 
+def get_fish_position(graph, y, x, di):
+    
+    positions = []
+    for _ in range(4):
+        y, x = y + move[di][0], x + move[di][1]
+
+        if not ((0 <= y < 4) and (0 <= x < 4)):
+            break
+        
+        if 1 <= graph[y][x][0] <= 16:
+            positions.append((y, x))
+        
+    return positions
+    
+
+def eat_fish(graph, y, x, total):
+    global result
+    
+    graph = copy.deepcopy(graph)
+    total += graph[y][x][0]
+    graph[y][x][0] = -2
+    
+    graph = move_fish(graph)
+    graph[y][x][0] = 0
+    
+    positions = get_fish_position(graph, y, x, graph[y][x][1])
+    
+    if not positions:
+        result = max(result, total)
+    
+    for y, x in positions:
+        eat_fish(graph, y, x, total)    
+    
 
 def main():
+    
     graph = get_input()
     for g in graph:
-        root_logger.info(f"{g}")
+        root_logger.debug(f"{g}")
 
-    graph[0][0][0] = -2
-    graph = move_fish(graph)
-
-    root_logger.info("after move fish")
-    for g in graph:
-        root_logger.info(f"{g}")
-
-
+    eat_fish(graph, 0, 0, 0)
+    print(result)
+    
+    
 if __name__ == "__main__":
     main()
