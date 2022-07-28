43ea539e98d486bac2e9ed303f0291d930b52adb
HyeonGeunY <hgyoon0928@gmail.com>
2022-07-28 13:13:14 +0900

    shark dinner

--- problems/baekjoon/shark_dinner.py
+++ problems/baekjoon/shark_dinner.py
@@ -1,17 +1,18 @@
 def get_input():
     n = int(input())
     sharks = [tuple(map(int, input().split(" "))) for _ in range(n)]
-    graph = [[] for _ in range(n)]
-        
-    return sharks, graph
+    
+    return sharks
 
 
-def make_graph(sharks, graph):
+def dinner(sharks):
     shark_map = [1] * len(sharks)
     shark_eat = [0] * len(sharks)
     
     for i in range(1, len(sharks)):
         for j in range(i):
+            if shark_map[j] == 0 or shark_map[i] == 0 or shark_eat[i] >= 2:
+                continue
             
             if (sharks[j][0] <= sharks[i][0] and sharks[j][1] <= sharks[i][1] and sharks[j][2] <= sharks[i][2]):
                 shark_map[j] = 0
@@ -21,13 +22,15 @@ def make_graph(sharks, graph):
 
 def main():
     
-    sharks, graph = get_input()
+    sharks = get_input()
     # print("shark")
     # print(f"{sharks}")
     
-    # print(sharks)
-    print(dinner(sharks))
-    print(graph)
+    sharks = sorted(sharks, key=lambda x: (x[0], x[1], x[2]))
     
+    # print(sharks)
+    
+    print(dinner(sharks))
+
 if __name__ == "__main__":
     main()
